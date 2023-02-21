bd139cfb4fb9e6608b3f43c3b2514383701f2392
icxrus <emppu.hietikko@gmail.com>
2023-02-21 14:22:39 +0200

    Completed module assignment 2

---
+++ Assets/Scripts/SpawnpointTrigger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class SpawnpointTrigger : MonoBehaviour
+{
+    public GameObject collectablePrefab;
+    GameObject go;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (go != null && go.transform.parent.CompareTag("Player"))
+            {
+                go.GetComponent<NetworkObject>().Despawn(destroy: true);
+
+            }
+
+            go = Instantiate(collectablePrefab, new Vector3(0, 0.5f, 0), Quaternion.identity);
+            go.GetComponent<NetworkObject>().Spawn();
+        }
+    }
+}
